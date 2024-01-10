c6ee5b6cec9ad9d04c3e2d1d32460de8ec16e3a4
opilane <opilane@HP-CZC2349HT4>
2024-01-10 12:43:06 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kutse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kutse()
+        {
+            int hour=DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust" : "Tere päevast";
+            ViewBag.Message = "Ootan sind oma peole! Tule kindlasti!!! Ootan sind!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

afa20121bacf928faa55e2bcbab75238107d6f95
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2024-02-05 14:06:20 +0200

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,71 @@
-﻿using System;
+﻿using Kutse_App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Kutse_App.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+
 
         public ActionResult Kutse()
         {
-            int hour=DateTime.Now.Hour;
+            
+            int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 10 ? "Tere hommikust" : "Tere päevast";
             ViewBag.Message = "Ootan sind oma peole! Tule kindlasti!!! Ootan sind!";
+            return View();
+        }
+        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust" : "Tere päevast";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ankeet()
+        {
 
             return View();
         }
-
+        [HttpPost]
+        public ViewResult Ankeet(Guest guest)
+        {
+            E_mail(guest);
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "stmp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "programmeriminevc@gmail.com";
+                WebMail.Password = "123456";
+                WebMail.From = "programmeriminevc@gmail.com";
+                WebMail.Send("matveikulakovski@gmail.com", "Vastus kutsele", guest.Name + "vastus" + ((guest.WillAttend ?? false) ?
+                    "tuleb peole" : "ei tule peole"));
+                ViewBag.Message = "Kiri on saatnud!";
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Mul on kahjul saa kirja saada!!!";
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
